e01408eec6b13cf1b348ad691e44ad020124c9a8
Vamsikrishna Chinthakayala <vamsikrishnach2000@gmail.com>
2025-12-13 13:56:11 +0530

    Fix: Corrected database password for user-service

--- payment-service/src/main/java/com/ecommerce/paymentservice/entity/PaymentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.paymentservice.entity;
-
-public enum PaymentStatus {
-    PENDING,
-    SUCCESS,
-    FAILED
-}
--- payment-service/src/main/java/com/ecommerce/paymentservice/repo/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.paymentservice.repo;
-
-import com.ecommerce.paymentservice.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
